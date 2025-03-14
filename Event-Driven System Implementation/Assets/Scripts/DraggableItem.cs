2d8b4e5ec861ee711b988364c72343da1b423a38
jgrypma99 <jgrypma99@gmail.com>
2025-03-14 00:10:30 -0400

    Changed from moving items around to cloning them to new slot

--- Event-Driven System Implementation/Assets/Scripts/DraggableItem.cs
+++ Event-Driven System Implementation/Assets/Scripts/DraggableItem.cs
@@ -7,8 +7,12 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
 {
     //allow dragging and dropping of items between inventory slots
     //taken from this tutorial: https://www.youtube.com/watch?v=kWRyZ3hb1Vc&t=149s
+    //modified for the purposes of this project to newGlyph an item instead of moving it
+    
     [HideInInspector ]public Transform parentAfterDrag;
     public Image image;
+    //newGlyph of the glyph to build the words
+    public GameObject newGlyph;
 
     public void Start()
     {
@@ -18,33 +22,42 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Begin drag");
+
+        //create a duplicate of the glyph
+        newGlyph = Instantiate(gameObject, transform.position, Quaternion.identity, transform.root);
+        newGlyph.GetComponent<Image>().raycastTarget = false; //prevent issues by disabling raycast
+        newGlyph.GetComponent<DraggableItem>().newGlyph = newGlyph; //maintian reference to new glyph
         
-        //change parent to canvas to allow for item to appear on top of everything else
-        parentAfterDrag = transform.parent;
-        transform.SetParent(transform.root);
-        transform.SetAsLastSibling();
-        
-        //hide item from raycasting during drag to enable moving to new slot
-        image.raycastTarget = false;
+        // Reset RectTransform to prevent scaling/position issues
+        RectTransform newGlyphRect = newGlyph.GetComponent<RectTransform>();
+        RectTransform originalRect = GetComponent<RectTransform>();
+
+        newGlyphRect.anchorMin = originalRect.anchorMin;
+        newGlyphRect.anchorMax = originalRect.anchorMax;
+        newGlyphRect.pivot = originalRect.pivot;
+        newGlyphRect.sizeDelta = originalRect.sizeDelta;
+
+        //change parent to canvas to appear on top
+        newGlyph.transform.SetAsLastSibling();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("Dragging");
 
-        //set position of item to posiition of mouse cursor
-        transform.position = Input.mousePosition;
+        //Move the new glyph
+        newGlyph.transform.position = Input.mousePosition;
+        
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("End drag");
         
-        //change parent to new slot
-        transform.SetParent(parentAfterDrag);
-
-        //re-enable raycasting to allow for dragging once again
-        image.raycastTarget = true;
+        if (newGlyph != null)
+        {
+            newGlyph.GetComponent<Image>().raycastTarget = true; //re-enable raycasting
+        }
     }
 
 }
--- Event-Driven System Implementation/Assets/Scripts/ItemSlots.cs
+++ Event-Driven System Implementation/Assets/Scripts/ItemSlots.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -8,9 +9,19 @@ public class ItemSlots : MonoBehaviour, IDropHandler
     //script to allow item to be moved to new slot upon release of drag/drop function
     public void OnDrop(PointerEventData eventData)
     {
-        GameObject dropeed = eventData.pointerDrag;
-        DraggableItem draggableItem = dropeed.GetComponent<DraggableItem>();
-        draggableItem.parentAfterDrag = transform;
+        GameObject draggedObject = eventData.pointerDrag;
+
+        if (draggedObject != null && transform.childCount == 0)
+        {
+            DraggableItem draggedItem = draggedObject.GetComponent<DraggableItem>();
+
+            if (draggedItem != null && draggedItem.newGlyph != null)
+            {
+                // Set the newGlyph as the child of the new slot
+                draggedItem.newGlyph.transform.SetParent(transform);
+                draggedItem.newGlyph.transform.localPosition = Vector3.zero; // Align properly
+            }
+        }
     }
 
 }
